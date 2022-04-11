c0b52d11d6d1e0dd6e84b92466378b6d3ae4fed5
SrNightmare09 <srnightmare09@gmail.com>
2022-04-11 14:36:21 +0530

    Add new file for for components

---
+++ Components.cs
@@ -0,0 +1,31 @@
+using System.Windows.Forms;
+using System.Drawing;
+
+public class Components {
+
+    public Form form = new Form();
+    public Panel panel = new Panel();
+
+    public Button newTaskButton = new Button();
+    public Button addTaskButton = new Button();
+    public Button exitButton = new Button();
+
+    public TextBox addTaskHeading = new TextBox();
+    public TextBox tasktitle = new TextBox();
+    public TextBox tasktitleHeading = new TextBox();
+    public TextBox taskdescHeading = new TextBox();
+    public TextBox taskdesc = new TextBox();
+    public TextBox suggestion1 = new TextBox(); // wow, such empty
+
+    public DataGridView table = new DataGridView();
+
+    public Color bgColor = ColorTranslator.FromHtml("#0F1821");
+    public Color textColor = ColorTranslator.FromHtml("#20C68D");
+
+    public int count = 1;
+
+    public string ID { get; set; }
+    public string TASK { get; set; }
+    public string DESCRIPTION { get; set; }
+
+}
